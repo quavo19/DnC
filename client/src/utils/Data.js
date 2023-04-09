4884f2b4fe5d131c096acccbf2a842369a544c97
Donald Akite <donaldakite27@gmail.com>
2023-04-09 13:10:42 +0000

    Add all json for screen

---
+++ client/src/utils/Data.js
@@ -0,0 +1,27 @@
+export const services = [
+    {
+        image: "images/service-02.png",
+        title: "it must make sence",
+        tagline: "just a tagline",
+    },
+    {
+        image: "images/service-03.png",
+        title: "it must make sence",
+        tagline: "just a tagline",
+    },
+    {
+        image: "images/service-04.png",
+        title: "it must make sence",
+        tagline: "just a tagline",
+    },
+    {
+        image: "images/service-05.png",
+        title: "it must make sence",
+        tagline: "just a tagline",
+    },
+    {
+        image: "images/service.png",
+        title: "it must make sence",
+        tagline: "just a tagline",
+    },
+]
