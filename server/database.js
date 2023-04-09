210db99032b224b5ef998e238e6c064a28ad56f3
Donald Akite <donaldakite27@gmail.com>
2023-04-09 13:05:55 +0000

    Add NoSQL database

---
+++ server/database.js
@@ -0,0 +1,18 @@
+// connection to our database.
+import mongoose from 'mongoose';
+
+const connectToDatabase = async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    const connect = await mongoose.connect('mongodb://0.0.0.0:27017/Today', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+export default connectToDatabase;
