0216db38a7c05a268f6505f086a1666154da8607
Donald Akite <donaldakite27@gmail.com>
2023-04-10 10:01:33 +0000

    Load product dynamically

--- client/src/components/SpecialProduct.jsx
+++ client/src/components/SpecialProduct.jsx
@@ -1,54 +1,34 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
-//import { Link } from "react-router-dom";
-const SpecialProduct = () => {
+import { Link } from "react-router-dom";
+const SpecialProduct = ({data}) => {
+  
   return (
     <>
-      <div className="col-6 mb-3">
+      <div className="special-width col-6 mb-3">
         <div className="special-product-card">
           <div className="d-flex justify-content-between">
-            <div>
-              <img src="images/watch.jpg" className="img-fluid" alt="watch" />
+            <div className="flash-container">
+              <img src={data.src} className="flash-sale img-fluid" alt="watch" />
             </div>
             <div className="special-product-content">
-              <h5 className="brand">Havels</h5>
+              <h5 className="brand">{data.brand}</h5>
               <h6 className="title">
-                Samsung Galaxy Note10+ Mobile Phone; Sim...
+                {data.title}
               </h6>
               <ReactStars
-                count={5}
-                size={24}
-                value={4}
-                edit={false}
-                activeColor="#ffd700"
+                count={data.ReactStars.count}
+                size={data.ReactStars.size}
+                value={data.ReactStars.value}
+                edit={data.ReactStars.edit}
+                activeColor={data.ReactStars.activeColor}
               />
               <p className="price">
-                <span className="red-p">$100</span> &nbsp; <strike>$200</strike>
+                <span className="red-p">GHC{data.price.sellingPrice}</span> &nbsp; <strike>GHC{data.price.originalPrice}</strike>
               </p>
-              <div className="discount-till d-flex align-items-center gap-10">
-                <p className="mb-0">
-                  <b>5 </b>days
-                </p>
-                <div className="d-flex gap-10 align-items-center">
-                  <span className="badge rounded-circle p-3 bg-danger">1</span>:
-                  <span className="badge rounded-circle p-3 bg-danger">1</span>:
-                  <span className="badge rounded-circle p-3 bg-danger">1</span>
-                </div>
-              </div>
-              <div className="prod-count my-3">
-                <p>Products: 5</p>
-                <div className="progress">
-                  <div
-                    className="progress-bar"
-                    role="progressbar"
-                    style={{ width: "25%" }}
-                    aria-valuenow="25"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  ></div>
-                </div>
-              </div>
-              {/* <Link className="button">Add to Cart</Link> */}
+              
+              
+              <Link to={'/'} className="home-buy">Add to Cart</Link>
             </div>
           </div>
         </div>
