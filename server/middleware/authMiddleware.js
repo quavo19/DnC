7fe5f727a8a1b165d4547b01aae86a631d3d414c
Donald Akite <donaldakite27@gmail.com>
2023-04-12 22:30:06 +0000

    Fix secrete token

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -10,7 +10,7 @@ const protectRoute = asyncHandler(async (req, res, next) => {
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
       token = req.headers.authorization.split(' ')[1];
-      const decoded = jwt.verify(token, 'process.env.TOKEN_SECRET');
+      const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
 
       req.user = User.findById(decoded.id);
 
