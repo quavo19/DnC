4d4eff90d47744528380431c2c8d8c94a8ccd140
Donald Akite <donaldakite27@gmail.com>
2023-05-08 13:29:58 +0000

    Format checkoutscreen

--- client/src/screens/CheckoutScreen.jsx
+++ client/src/screens/CheckoutScreen.jsx
@@ -1,8 +1,8 @@
-import { Box, Heading, Stack, Flex } from '@chakra-ui/react';
-import { useSelector } from 'react-redux';
-import { Navigate, useLocation } from 'react-router-dom';
-import CheckoutOrderSummary from '../components/CheckoutOrderSummary';
-import ShippingInformation from '../components/ShippingInformation';
+import { Box, Heading, Stack, Flex } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
+import CheckoutOrderSummary from "../components/CheckoutOrderSummary";
+import ShippingInformation from "../components/ShippingInformation";
 
 const CheckoutScreen = () => {
   const user = useSelector((state) => state.user);
@@ -11,28 +11,35 @@ const CheckoutScreen = () => {
 
   return userInfo ? (
     <Box
-      minH='100vh'
-      maxW={{ base: '3xl', lg: '7xl' }}
-      mx='auto'
-      px={{ base: '4', md: '8', lg: '12' }}
-      py={{ base: '6', md: '8', lg: '12' }}>
-      <Stack direction={{ base: 'column', lg: 'row' }} align={{ lg: 'flex-start' }}>
-        <Stack spacing={{ base: '8', md: '10' }} flex='1.5' mb={{ base: '12', md: 'none' }}>
-          <Heading fontSize='2xl' fontWeight='extrabold'>
+      minH="100vh"
+      maxW={{ base: "3xl", lg: "7xl" }}
+      mx="auto"
+      px={{ base: "4", md: "8", lg: "12" }}
+      py={{ base: "6", md: "8", lg: "12" }}
+    >
+      <Stack
+        direction={{ base: "column", lg: "row" }}
+        align={{ lg: "flex-start" }}
+      >
+        <Stack
+          spacing={{ base: "8", md: "10" }}
+          flex="1.5"
+          mb={{ base: "12", md: "none" }}
+        >
+          <Heading fontSize="2xl" fontWeight="extrabold">
             Shipping Information
-            
           </Heading>
-          <Stack spacing='6'>
+          <Stack spacing="6">
             <ShippingInformation />
           </Stack>
         </Stack>
-        <Flex direction='column' align='center' flex='1'>
+        <Flex direction="column" align="center" flex="1">
           <CheckoutOrderSummary />
         </Flex>
       </Stack>
     </Box>
   ) : (
-    <Navigate to='/login' replace={true} state={{ from: location }} />
+    <Navigate to="/login" replace={true} state={{ from: location }} />
   );
 };
 
