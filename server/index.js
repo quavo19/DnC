3e13435c1612a68b738c5c5ad683c33477f6506c
Donald Akite <donaldakite27@gmail.com>
2023-05-10 02:33:03 +0000

    Add headers

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,  Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   next();
 });
 app.use("/api/products", productRoutes);
